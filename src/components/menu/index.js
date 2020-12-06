5faf02048fe7312a65b3e5cb9bcb944fa1426f23
Vladimir <mr.qrsant@gmail.com>
2020-12-06 17:49:47 +0300

    Начата работа над меню

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,11 +1,28 @@
-
 export default class Menu {
 	constructor() {}
 
 	init() {
 		this.menu = document.querySelector('.menu');
-		if (!this.menu) return false;
+		if (this.menu) {
+			this.addListeners();
+		} else {
+			return false;
+		}
+	}
 
 
+	addListeners() {
+		console.log('Добавляем слушателей');
+		// Основные title
+		const menuMainTitlesArr = document.getElementsByClassName('title__main');
+
+		menuMainTitlesArr.forEach(element => {
+			element.addEventListener('click', (event) => {
+				const clickedElement = event.currentTarget.getAttribute('data-id');
+				// const countryNumber = parseInt(clickedElement.getAttribute('id').split('_').pop(), 10);
+		
+				console.log(clickedElement);
+			  });
+		});
 	}
 }
